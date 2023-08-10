cb2571264c5af4cd011b54976559f9e03a01da64
Arhey- <Arhey-@users.noreply.github.com>
2023-08-10 03:09:45 +0300

    links: eval, prev diff

--- temp-links.js
+++ temp-links.js
@@ -1,11 +1,12 @@
-(()=>{
+(() => {
 	const w = open('https://arhey-.github.io/memman/temp-links.html');
 	addEventListener('message', function wait(e) {
+		let d = document, $ = s => d.querySelector;
 		if (e.data == 'ready') {
 			w.postMessage({
 				url: location.href,
-				title: document.querySelector('meta[property="og:title"]')?.content || document.title,
-				src: document.querySelector('meta[property="og:image"]')?.content,
+				name: $('meta[property="og:title"]')?.content || d.title,
+				src: $('meta[property="og:image"]')?.content,
 			}, '*');
 			removeEventListener('message', wait);
 		}
