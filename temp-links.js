239ecf6c3130bf6296b1bc50f73469211986cc8f
Arhey- <Arhey-@users.noreply.github.com>
2023-08-12 23:44:46 +0300

    links: [wip] images

--- temp-links.js
+++ temp-links.js
@@ -1,12 +1,15 @@
 (() => {
-	const w = open('https://arhey-.github.io/memman/temp-links.html');
+	const w = open('https://arhey-.github.io/memman/links.html');
 	addEventListener('message', function wait(e) {
-		let d = document, $ = s => d.querySelector;
+		let d = document, $ = s => d.querySelector(s);
 		if (e.data == 'ready') {
+			const src = $('meta[property="og:image"]')?.content;
+			const srcs = src ? [] : [...d.querySelectorAll('img')].map(i => i.src);
 			w.postMessage({
 				url: location.href,
 				name: $('meta[property="og:title"]')?.content || d.title,
-				src: $('meta[property="og:image"]')?.content,
+				src,
+				srcs,
 			}, '*');
 			removeEventListener('message', wait);
 		}
