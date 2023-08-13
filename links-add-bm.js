8aa90dd753529c241f2c1c8a5afd3450d4c85605
Arhey- <Arhey-@users.noreply.github.com>
2023-08-14 02:23:04 +0300

    links: exclude tags

--- links-add-bm.js
+++ links-add-bm.js
@@ -3,8 +3,13 @@
 	addEventListener('message', function wait(e) {
 		let d = document, $ = s => d.querySelector(s);
 		if (e.data == 'ready') {
-			const src = $('meta[property="og:image"]')?.content;
-			const srcs = src ? [] : [...d.querySelectorAll('img')].map(i => i.src);
+			let src = $('meta[property="og:image"]')?.content, srcs = [];
+			if (!src) srcs = [...d.querySelectorAll('img')]
+				.filter(i => {
+					let r = i.getBoundingClientRect();
+					return r.bottom >= 0 && r.top <= screen.height
+				})
+				.map(i => i.src);
 			w.postMessage({
 				url: location.href,
 				name: $('meta[property="og:title"]')?.content || d.title,
